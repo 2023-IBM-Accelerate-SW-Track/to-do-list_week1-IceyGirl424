45a25ce6638fa66bf6b5b2d333b3ea30f97a897c
Lena Munad <munadlena@gmail.com>
2023-06-10 16:07:25 -0700

    Made necessary changes to update About and To-Do

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
+import "./About.css";
+import zhongliii from "../assets/zhongliii.jpeg";
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+         <div>
+    <div className="split left">
+      <div className="centered">
+        <img 
+          className="profile_image"
+          src={zhongliii}
+          alt="Profile Pic"
+          ></img>
+      </div>
+    </div>
+    <div className="split right">
+      <div className="centered">
+        <div className="name_title">Your Name</div>
+        <div className="brief_description">
+          Hello! My name is Lena Munad, and I'm a Computer Science at LPC. 
+        </div>
+      </div>
+    </div>
+  </div>
+
       </div>
     )
   }
